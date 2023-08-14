3704a3ef0d575cb2bd190ba6fffa42b3dffdc6a1
Zechariah Gaiya <abbazechariah@gmail.com>
2023-08-14 08:39:13 -0700

    i don tire for error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-  printf("Size of a char: %n byte(s)\n", sizeof(char));
-  printf("Size of an int: %n byte(s)\n", sizeof(int));
-  printf("Size of a long int: %n byte(s)\n", sizeof(long int));
-  printf("Size of a long long int: %n byte(s)\n", sizeof(long long int));
-  printf("Size of a float: %n byte(s)\n", sizeof(float));
-  return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
