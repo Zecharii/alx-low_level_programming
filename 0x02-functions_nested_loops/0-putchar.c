beed3cf5afc77a8afd834be0d5194c8f3cfc1343
Zechariah Gaiya <abbazechariah@gmail.com>
2023-08-15 11:43:22 -0700

    Minor changes to 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * This is the main function of the program. It serves as the entry point
- * for program execution. It may call other functions or perform various
- * tasks based on the program's requirements.
+ * main - Prints _putchar as a message.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    printf("_putchar\n");
-    return 0;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
