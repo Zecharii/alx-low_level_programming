801128bb1aa2b6c9f42694134c5a87957eab8244
Zechariah Gaiya <abbazechariah@gmail.com>
2023-08-16 04:24:58 -0700

    Adding 10-add.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,14 +14,14 @@ void times_table(void)
     {
       for (y = 0; y <= 9; y++)
 	{
-	  z = x *y;
+	  z = x * y;
 	  if (z > 9)
 	    {
 	      u = z % 10;
 	      d = (z - u) / 10;
 	      _putchar(44);
 	      _putchar(32);
-	      _putchar(d + 'a');
+	      _putchar(d + '0');
 	      _putchar(u + '0');
 	    }
 	  else
