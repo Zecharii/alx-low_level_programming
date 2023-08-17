f0b1ce5414acdcd3be1c0a5227b0389218e5ddb2
Zechariah Gaiya <abbazechariah@gmail.com>
2023-08-17 09:49:24 -0700

    change is a must

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,15 @@
  * print_numbers - prints numbers between 0 to 9.
  * Return: no return.
  */
+
 void print_numbers(void)
 {
-  int ch;
+	int ch;
 
-  for (ch = 0; ch <= 9; ch++)
-    {
-      _putchar(ch + '0');
-    }
-  _putchar('\n');
+	for (ch = 0; ch <= 9; ch++)
+	{
+		_putchar(ch + '0');
+	}
+
+	_putchar('\n');
 }
