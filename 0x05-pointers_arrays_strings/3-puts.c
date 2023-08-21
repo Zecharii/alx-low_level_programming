463f413e6699d7daa04443872f8d06cb1a21ac9e
Zechariah Gaiya <abbazechariah@gmail.com>
2023-08-21 11:54:59 -0700

    Adding 100-atoi.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,18 +6,12 @@
  * @str: input string.
  * Return: no return.
  */
+
 void _puts(char *str)
 {
-	int count = 0;
-
-	while (count >= 0)
+	for (; *str != '\0'; str++)
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		_putchar(*str);
 	}
+	_putchar('\n');
 }
