c1b45cb93a2d55c29f3b1237378f1d19e8db5395
Zechariah Gaiya <abbazechariah@gmail.com>
2023-08-24 11:03:25 -0700

    Adding 4-rev_array.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio>
+#include <stdio.h>
 
 /**
  * _strcmp - compares two strings.
@@ -9,17 +9,17 @@
  * Return: 0 if s1 == s2, else the number if not.
  */
 
-int _strcmp(char * s1, char * s2)
+int _strcmp(char *s1, char *s2)
 
 {
-	while ( * s1 != '\0' && * s2 != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if ( * s1 != * s2)
+        	if (*s1 != *s2)
 		{
-			return ( * s1 - * s2);
+			return (*s1 - *s2);
 		}
 		s1++;
 		s2++;
 	}
-	return ( * s1 - * s2);
+	return (*s1 - *s2);
 }
