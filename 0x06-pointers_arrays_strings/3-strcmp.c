da74636e65312ac73b14e40bff5298fadec77d77
Zechariah Gaiya <abbazechariah@gmail.com>
2023-08-24 11:05:40 -0700

    minor changes to 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 
 /**
- * _strcmp - compares two strings.
+ * _strcmp - compares two strings
  * @s1: first string.
  * @s2: second string.
- *
- * Return: 0 if s1 == s2, else the number if not.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
  */
 
 int _strcmp(char *s1, char *s2)
-
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	int i = 0, op = 0;
+
+	while (op == 0)
 	{
-        	if (*s1 != *s2)
-		{
-			return (*s1 - *s2);
-		}
-		s1++;
-		s2++;
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
 	}
-	return (*s1 - *s2);
+
+	return (op);
 }
