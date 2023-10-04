c33d97daa05aee615ad7ded81c37de64ef6928cf
Zechariah Gaiya <abbazechariah@gmail.com>
2023-10-04 03:00:06 -0700

    Adding 1-create_file.c again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,45 +1,38 @@
 #include "main.h"
 
 /**
- * creat_file - creates a file and writes text content to it
- * @filename: the name of the file to create
- * @text_content: the string to write to the file
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 if it success. -1 if it fails.
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int pletters;
-	int wrr
-	
-	if (filename == NULL)
-	{
+	int nletters;
+	int rwr;
+
+	if (!filename)
 		return (-1);
-	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
-	{
 		return (-1);
-	}
 
-	if (text_content != NULL)
-	{
-		pletters = 0;
-		while (text_content[pletters])
-			pletters++;
+	if (!text_content)
+		text_content = "";
 
-		wrr = write(fd, text_content, pletters);
-		if (wrr == -1)
-		{
-			close(fd);
-			return(-1);
-		}
-	}
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
+
 	return (1);
 }
