98272b7d849be72884a4e1be77effdeb5ffd01eb
Zechariah Gaiya <abbazechariah@gmail.com>
2023-10-30 09:44:59 -0700

    Adding README.md, main.h, 0-main.c, 1-main.c, 2-main.c, and 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX std output.
+ * @filename: name of the file to read.
+ * @letters: number of letters to read and print.
+ * Return: number of letters it could read and print, 0 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	ssize_t read_result, write_result;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (buffer == NULL)
+		return (0);
+
+	read_result = read(file_descriptor, buffer, letters);
+	if (read_result == -1)
+		close(file_descriptor);
+		return (0);
+
+	write_result = write(file_descriptor, buffer, read_result);
+	if (write_result == -1 || read_result != write_result)
+		close(file_descriptor);
+		return (0);
+
+	close(file_descriptor);
+
+	free(buffer);
+
+	return (read_result);
+}
